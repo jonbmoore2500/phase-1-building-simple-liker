45dba2249d1c5da29642fa6199bdc8878f86619b
Jonathan <jonathanmoorevla@gmail.com>
2022-07-10 17:30:35 -0500

    commit tree 8dd47ddd582be6fbec2637ed555b2a9565ba7db2

--- main.js
+++ main.js
@@ -5,6 +5,45 @@ const FULL_HEART = '♥'
 // Your JavaScript code goes here!
 
 
+// creates event listener for each heart with class 'like-glyph', 
+// runs heartSwitch() on whichever is clicked
+function clicking() {
+  document.querySelectorAll('.like-glyph').forEach(heart => {
+  heart.addEventListener('click', (e) => {   
+    heartSwitch(e);
+  }
+  )
+}
+)}
+
+// handles server call and switching hearts
+// calls mimicServerCall, if successful triggers .then and switches hearts
+// if unsuccessful triggers .catch and goes to handleError function (see below)
+function heartSwitch(e) {
+  mimicServerCall().then(() => {
+    //console.log('confirmation')
+  if (e.target.textContent === EMPTY_HEART) {
+    e.target.textContent = FULL_HEART
+    e.target.className = 'activated-heart'
+  } else if (e.target.textContent === FULL_HEART) {
+    e.target.textContent = EMPTY_HEART
+    e.target.className = ''
+  }}
+  )
+  .catch(() => handleError())
+}
+
+// fills out handleError function for .catch
+// if catch triggers, unhides error message then rehides after 3 secs
+function handleError() {
+  document.querySelector('#modal').className = '';
+  setTimeout(() => {
+    document.querySelector('#modal').className = 'hidden'}, 3000)
+}
+
+// invokes clicking function for everything to run
+clicking();
+
 
 
 //------------------------------------------------------------------------------
